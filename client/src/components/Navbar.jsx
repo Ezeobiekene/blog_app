6d8ce5f38038437b21b3b201cd79e0ed43d4ea49
Ekene Ezeobi <ezeobiekene7@gmail.com>
2024-11-05 18:25:11 -0500

    added file content

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className="container">
+        <div className="logo">logo</div>
+        <div className="links">links</div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
