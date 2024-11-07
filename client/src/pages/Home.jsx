adf40db3ac380c418e2c28d365ac5dc52ae72cfe
Ekene Ezeobi <ezeobiekene7@gmail.com>
2024-11-07 17:23:29 -0500

    styled home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
               <img src={post.img} alt="" />
             </div>
             <div className="content">
-              <Link to={`/post/${post.id}`}>
+              <Link className="link" to={`/post/${post.id}`}>
                 <h1>{post.title}</h1>
               </Link>
               <p>{post.desc}</p>
